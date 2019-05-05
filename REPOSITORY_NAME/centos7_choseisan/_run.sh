2dc0b585541299bdaf74266d3b77740908b97623
s-kono <s-kono@outlook.com>
2019-05-06 04:45:34 +0900

    update _run.sh

--- REPOSITORY_NAME/centos7_choseisan/_run.sh
+++ REPOSITORY_NAME/centos7_choseisan/_run.sh
@@ -13,7 +13,9 @@ readonly IMAGE_NAME=$( echo ${DIR} | awk -F/ '{print $NF}' )
 readonly REPO_NAME=$( echo ${DIR} | perl -pe 's%^.*/(.+)/'${IMAGE_NAME}'%$1%' )
 
 set -x
-#sudo docker run --net=none                -h ${IMAGE_NAME} -it --rm ${REPO_NAME}/${IMAGE_NAME}
+#sudo docker run --net=none                -h ${IMAGE_NAME} -it --rm ${REPO_NAME}/${IMAGE_NAME} /bin/bash
 #sudo docker run --net=bridge              -h ${IMAGE_NAME} -it --rm ${REPO_NAME}/${IMAGE_NAME}
  sudo docker run --net=bridge -p 3000:3000 -h ${IMAGE_NAME} -it --rm ${REPO_NAME}/${IMAGE_NAME}
+#sudo docker run --net=bridge -p 8000:80   -h ${IMAGE_NAME} -it --rm ${REPO_NAME}/${IMAGE_NAME}
+#sudo docker run --net=bridge -p 8080:81   -h ${IMAGE_NAME} -it --rm ${REPO_NAME}/${IMAGE_NAME}
 #sudo docker run --net=host                -h ${IMAGE_NAME} -it --rm ${REPO_NAME}/${IMAGE_NAME}
