7ef3bae94ad22e6ef5df38ebf9bc0d3c484479e1
s-kono <s-kono@outlook.com>
2019-05-08 00:46:24 +0900

    add forward_proxy/

---
+++ REPOSITORY_NAME/forward_proxy/_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -u
+
+export LANG=C
+export LC_CTYPE=en_US.UTF-8
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+#readonly FULL0=$( readlink -f $0 )
+readonly FULL0=$( cd $( dirname $0 ) && echo $( pwd )/$( basename $0 ) )
+readonly DIR=$( dirname ${FULL0} )
+
+readonly IMAGE_NAME=$( echo ${DIR} | awk -F/ '{print $NF}' )
+readonly REPO_NAME=$( echo ${DIR} | perl -pe 's%^.*/(.+)/'${IMAGE_NAME}'%$1%' )
+
+set -x
+#sudo docker run --net=none              -h ${IMAGE_NAME} -it --rm ${REPO_NAME}/${IMAGE_NAME} /bin/bash
+#sudo docker run --net=bridge            -h ${IMAGE_NAME} -it --rm ${REPO_NAME}/${IMAGE_NAME}
+#sudo docker run --net=bridge -p 8000:80 -h ${IMAGE_NAME} -it --rm ${REPO_NAME}/${IMAGE_NAME}
+ sudo docker run --net=bridge -p 8080:88 -h ${IMAGE_NAME} -it --rm ${REPO_NAME}/${IMAGE_NAME}
+#sudo docker run --net=host              -h ${IMAGE_NAME} -it --rm ${REPO_NAME}/${IMAGE_NAME}
